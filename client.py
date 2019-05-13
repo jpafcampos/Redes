69b65c691ecf452955ef3a64c333b2bf158357c6
Dominique Auer <dq.auer@gmail.com>
2019-05-13 19:40:13 -0300

    erros atuais: servidor e cliente travam depois do fim da primeira requisição; servidor não achou uma palavra que estava no arquivo falta testar: se o txt dicio está realmente adequado;

--- client.py
+++ client.py
@@ -3,15 +3,24 @@ from socket import *
 serverName = 'servername'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect( ('', 1234) )
-palavra = input('Digite uma palavra:')
-clientSocket.send(palavra.encode())
-l1 = input('Indique o idioma de origem: (en, fr, pt)' )
-clientSocket.send(l1.encode())
-l2 = input('Indique o idioma destino:')
-clientSocket.send(l2.encode())
-traducao = clientSocket.recv(1024).decode()
+clientSocket.connect( ('192.168.0.14', 8888) ) 
+#clientSocket.connect( ('', 1234) )
 
-print ('Tradução:', traducao)
+fim = 0;
+while(fim == 0):
+	palavra = input('Digite uma palavra:')
+	clientSocket.send(palavra.encode())
+	l1 = input('Indique o idioma de origem: (en, es, pt, fr, de, zu, eo, it) ' )
+	clientSocket.send(l1.encode())
+	l2 = input('Indique o idioma destino:')
+	clientSocket.send(l2.encode())
+	traducao = clientSocket.recv(1024).decode()
+	print ('Tradução:', traducao)
+	
+	ans = input('Deseja continuar? (s/n) ')
+	if(ans == "n"):
+		fim = 1
+
+	clientSocket.send(ans.encode())
 
 clientSocket.close()
--- server.py
+++ server.py
@@ -5,13 +5,23 @@ import numpy as np
 def translate (word, language_1, language_2):
     #traduz "word" da "language1" para a "language2"
 
-    dic = open('dictionary.txt', 'r')
+    dic = open('dicio.txt', 'r', encoding = "utf8")
     if language_1 == "en":
         l1 = 0
     elif language_1 == "pt":
         l1 = 1
     elif language_1 == "fr":
         l1 = 2
+    elif language_1 == "es":
+        l1 = 3
+    elif language_1 == "it":
+        l1 = 4
+    elif language_1 == "de":
+        l1 = 5
+    elif language_1 == "zu":
+        l1 = 6
+    elif language_1 == "eo":
+        l1 = 7
 
     if language_2 == "en":
         l2 = 0
@@ -19,17 +29,39 @@ def translate (word, language_1, language_2):
         l2 = 1
     elif language_2 == "fr":
         l2 = 2
+    elif language_2 == "es":
+        l2 = 3
+    elif language_2 == "it":
+        l2 = 4
+    elif language_2 == "de":
+        l2 = 5
+    elif language_2 == "zu":
+        l2 = 6
+    elif language_2 == "eo":
+        l2 = 7
+
 
     en = []
     pt = []
     fr = []
+    es = []
+    it = []
+    de = []
+    zu = []
+    eo = []
+
     for line in dic:
         colunas = line.split() #separa as linhas do arquivo
         en.append(colunas[0]) #lista com palavras em ingles
-        pt.append(colunas[1])
-        fr.append(colunas[2])
+        pt.append(colunas[1]) #pt
+        fr.append(colunas[2]) #fr
+        es.append(colunas[3]) #es
+        it.append(colunas[4]) #it
+        de.append(colunas[5]) #de
+        zu.append(colunas[6]) #zu
+        eo.append(colunas[7]) #eo
 
-    matrix = np.array([en,pt,fr])
+    matrix = np.array([en,pt,fr, es, it, de, zu, eo])
 
     position = 0
     w = word
@@ -41,14 +73,15 @@ def translate (word, language_1, language_2):
 
     else:
         notFound = 1
-        return notFound
+        return 'Palavra não encontrada. Tente novamente.'
     
     return matrix[l2][p]
 #fim "translate"
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind ( ('', 1234) )
+serverSocket.bind ( ('192.168.0.14', 8888) )
+#serverSocket.bind ( ('', 1234) )
 serverSocket.listen(1)
 
 print ('Servidor pronto')
@@ -62,4 +95,6 @@ while 1:
     l2 = connectionSocket.recv(1024).decode()
     traducao = translate(palavra, l1, l2)
     connectionSocket.send(traducao.encode())
-    connectionSocket.close()
+    ans = connectionSocket.recv(1024).decode()
+    if ans == "n":
+        connectionSocket.close()
